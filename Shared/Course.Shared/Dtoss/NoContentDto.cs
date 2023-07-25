543b6de61396da55e10f5f99434d2c2f93c67a7f
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-25 14:14:01 +0300

    Add : Shared project to reference from other microservice APIs

---
+++ Shared/Course.Shared/Dtoss/NoContentDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Shared.Dtoss
+{
+    // başarılı Response için, geriye bir veri dönmek istemediğim zaman, T ile dönebilmek için bu sınıfı oluşturduk
+    public class NoContentDto
+    {
+    }
+}
---
+++ Shared/Course.Shared/Dtoss/ResponseDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Course.Shared.Dtoss
+{
+    public class ResponseDto<T>
+    {
+        public T Data { get; private set; }
+
+        [JsonIgnore] // statuscode zaten response ile dönüyor, bunu içeride kendim kullanıcam, response bodysinde bir daha belirtmeme gerek yok.
+        public int StatusCode { get; private set; }
+
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+
+        public List<string> Errors { get; set; }
+
+        //Static Factory Method
+        public static ResponseDto<T> Success(T data, int statusCode)
+        {
+            return new ResponseDto<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static ResponseDto<T> Success(int statusCode)
+        {
+            return new ResponseDto<T> { Data = default, StatusCode = statusCode, IsSuccessful = true };
+        } 
+        
+        public static ResponseDto<T> Fail(List<string> errors,int statusCode)
+        {
+            return new ResponseDto<T> { Data = default, StatusCode = statusCode, IsSuccessful = false, Errors = errors };
+        }
+
+        public static ResponseDto<T> Fail(string error,int statusCode)
+        {
+            return new ResponseDto<T> { Data = default, StatusCode = statusCode, IsSuccessful = false, Errors = new List<string> { error} };
+        }
+    }
+}
