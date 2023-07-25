f8b2e68a6a6f25125f51ea3dd633d7dcea078c85
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-25 13:45:01 +0300

    initial
    Couse application using microservices
    Add: Catalog.API projeect and its models

---
+++ Services/Catalog/Course.Services.Catalog/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Course.Services.Catalog.Models
+{
+    public class Category
+    {
+        [BsonId] //Mongo bu Id
+        [BsonRepresentation(BsonType.ObjectId)] // mongoda Id tipi ObjectId
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
