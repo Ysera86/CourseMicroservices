d7d4ebd917757b0b18faf97d84f5ca6316cd8b3e
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-25 15:31:12 +0300

    Add : Mongodb DatabaseSettings implement via OptionsPattern

--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,16 +1,39 @@
 using AutoMapper;
 using FreeCourse.Services.Catalog.Mapping;
+using FreeCourse.Services.Catalog.Settings;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//..
+//OptionsPattern
+
+/*
+ IOptions<DatabaseSettings> options
+
+options.Value 
+
+þeklinde kullanabilirdim ama classlarýnctoruna DI olarak IOptions geçmek yerine OptionsPattern ile DatabaseSettings ve IDatabaseSettings oluþturup okuyup DI olarak IDatabaseSettings geçebilirim artýk
+ */
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
+
+
+//..
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAutoMapper(typeof(GeneralMapping));
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
