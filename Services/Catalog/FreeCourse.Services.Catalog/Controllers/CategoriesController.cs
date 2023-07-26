24625a8dfde54b5450e06e10ad3a7d2ba2d122e6
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-26 15:58:18 +0300

    Modify : Fixed all errors. Internal > public for all classes or swagger not displays endpoints naturally All all http methods , modify route add [action] add missing mapping Test : All the endpoints

--- Services/Catalog/FreeCourse.Services.Catalog/Controllers/CategoriesController.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CategoriesController.cs
@@ -6,9 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FreeCourse.Services.Catalog.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
-    internal class CategoriesController : CustomBaseController
+    public class CategoriesController : CustomBaseController
     {
         private readonly ICategoryService _categoryService;
 
@@ -17,6 +17,7 @@ namespace FreeCourse.Services.Catalog.Controllers
             _categoryService = categoryService;
         }
 
+        [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             var response = await _categoryService.GetAllAsync();
--- Services/Catalog/FreeCourse.Services.Catalog/Dtos/CategoryCreateDto.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CategoryCreateDto.cs
@@ -3,9 +3,8 @@ using MongoDB.Bson;
 
 namespace FreeCourse.Services.Catalog.Dtos
 {
-    internal class CategoryCreateDto
+    public class CategoryCreateDto
     {
-        public string Id { get; set; }
         public string Name { get; set; }
     }
 }
