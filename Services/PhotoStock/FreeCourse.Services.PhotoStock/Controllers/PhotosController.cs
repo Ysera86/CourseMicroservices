4c675b03465b6a0c9e784471897635d9881124ff
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-27 14:03:47 +0300

    Add : Implement PhotosController, save and delete endpoints

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,57 @@
+﻿using FreeCourse.Services.PhotoStock.Dtos;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        /// <summary>
+        /// CancellationToken, foto diyelim 30 snde kaydoluyor, 10. saniyede kullanıcı tarayıcıyı kapatırsa otomatik olarak CancellationToken tetiklenir ve bu işlemi durdurur. Bu olmazsa asenkron da olduğu için arkada bir süre daha devam ederdi.
+        /// Asenkron bir işlemi ancak hata fırlatarak sonlandırabiliriz -CancellationToken da hata fırlatarak işlemi sonlandırıyor.
+        /// </summary>
+        /// <param name="photo"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> PhotosSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo != null && photo.Length > 0)
+            {
+                // random isim oluşturmak daha iyi olurdu
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using var stream = new FileStream(path, FileMode.Create);
+                await photo.CopyToAsync(stream, cancellationToken);
+
+                // http://www.photostock.api.com/photos.asdljasıda.jpg
+                var returnPath = $"photos/{photo.FileName}";
+
+                PhotoDto photoDto = new() { Url = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, StatusCodes.Status200OK));
+            }
+
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty", StatusCodes.Status400BadRequest));
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwwroot/photos", photoUrl);
+
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo not found", StatusCodes.Status404NotFound));
+            }
+
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(StatusCodes.Status204NoContent));
+        }
+    }
+}
