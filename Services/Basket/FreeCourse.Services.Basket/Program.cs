50eaca9cb1a685f7c89c14f477541f727c6baa43
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-28 11:42:36 +0300

    Add : RedisSettings class to read host and port from appsettings via OptionsPattern

--- Services/Basket/FreeCourse.Services.Basket/Program.cs
+++ Services/Basket/FreeCourse.Services.Basket/Program.cs
@@ -1,6 +1,25 @@
+using FreeCourse.Services.Basket.Settings;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//.
+
+#region OptionsPattern ile RedisSettings okuma
+
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
+builder.Services.AddSingleton<IRedisSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<RedisSettings>>().Value;
+}); 
+
+#endregion
+
+//..
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
