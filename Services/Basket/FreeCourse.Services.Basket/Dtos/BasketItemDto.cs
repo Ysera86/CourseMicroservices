20b0e3147d7aa741f6d03b4236bd101fc1a8993c
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-28 11:32:50 +0300

    Add : models

---
+++ Services/Basket/FreeCourse.Services.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FreeCourse.Services.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        public int Quantity { get; set; } // aslında 1 kurstan daha fazla alınmaz ama e-ticaret örnekli olsun
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal Price{ get; set; }
+    }
+}
